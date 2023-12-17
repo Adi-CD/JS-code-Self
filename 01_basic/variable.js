6850c3099d099ade668e00c54a799a23e07c8a08
Aditya Rajoria <154041297+Adi-CD@users.noreply.github.com>
2023-12-17 11:40:54 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,18 @@
+const accountId =143352; // it does not change 
+let accountEmail="aditya@gmail.com"
+var accountPassword ="12ditya"
+
+
+//defining the new value
+ accountEmail=" gcg.a@gmail"
+ accountPassword="hufddd"
+ let state;
+
+/*
+not using the var 
+bcoz of block scope and functional scope
+*/
+
+
+//console.log(accountId); // if you want to print one thing
+console.table([accountId , accountEmail ,accountPassword ,state])  // if you want to print multiple values at once
