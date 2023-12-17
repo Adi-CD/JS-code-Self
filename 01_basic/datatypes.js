446baaaae13023ffa0ab3ba4fce3ce1f5698ec88
Aditya Rajoria <154041297+Adi-CD@users.noreply.github.com>
2023-12-17 12:23:25 +0000

    container config done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,14 @@
+//primitive datatypes
+let age=78
+let name="ADITYA"
+let isLogged=true;
+
+//number
+//string
+//null
+//boolean
+//bgint ==> for big numbers
+//symbol ==> uniqueness
+//typeof ==> wheere we know the datatype
+
+console.table([age,name,isLogged])
