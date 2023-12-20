2ab51a704b863a8b4d555d727fbeef05ff3ca2fa
Aditya Rajoria <154041297+Adi-CD@users.noreply.github.com>
2023-12-20 11:26:15 +0000

    06_comparison done

---
+++ 01_basic/06_comparison.js
@@ -0,0 +1,22 @@
+//but these all are simple comaprisons 
+console.log(2<1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+console.log(2>=1)
+
+//when we doing the differnt datatype comparison so 
+//so we dont get the predictable output
+
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
+
+//coparisom of both is false 
+//but if we add the add the == so it gives the true
+//in which null is converted and treated as 0
+
+//=== strict check it also check the datatype also
+console.log("2"==1);
+console.log(2===2);
+//in this ==strict check 
